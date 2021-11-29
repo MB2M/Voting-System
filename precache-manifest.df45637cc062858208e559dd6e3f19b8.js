0a062804ff931ecc466f4870ea429b57b8d9f5aa
MB2M <damsbooster@gmail.com>
2021-11-29 16:28:06 +0100

    Updates

---
+++ precache-manifest.df45637cc062858208e559dd6e3f19b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "739b5f712df7270ffe22916b15ed07f5",
+    "url": "/Voting-System/index.html"
+  },
+  {
+    "revision": "ab9409388e1c428669af",
+    "url": "/Voting-System/static/css/2.cdbe2572.chunk.css"
+  },
+  {
+    "revision": "857780c7ca6d59a5cfdd",
+    "url": "/Voting-System/static/css/main.730fdd82.chunk.css"
+  },
+  {
+    "revision": "ab9409388e1c428669af",
+    "url": "/Voting-System/static/js/2.0e94ec70.chunk.js"
+  },
+  {
+    "revision": "857780c7ca6d59a5cfdd",
+    "url": "/Voting-System/static/js/main.8dcaec33.chunk.js"
+  },
+  {
+    "revision": "a8db6408b825a5fe0dfc",
+    "url": "/Voting-System/static/js/runtime-main.e1faef09.js"
+  }
+]);
