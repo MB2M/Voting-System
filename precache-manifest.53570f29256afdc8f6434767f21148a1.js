2e3657edef97d1b36ff1151b52ad82f57c68c7f2
MB2M <damsbooster@gmail.com>
2021-11-29 16:41:58 +0100

    Updates

--- precache-manifest.53570f29256afdc8f6434767f21148a1.js
+++ precache-manifest.53570f29256afdc8f6434767f21148a1.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0261142058cf202f88a0482c9c4f251e",
+    "revision": "29a6cdab359cfcd5b027dfc8d3ab4b9c",
     "url": "/Voting-System/index.html"
   },
   {
@@ -8,19 +8,23 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Voting-System/static/css/2.7020638d.chunk.css"
   },
   {
-    "revision": "e44accd67aff01e00abb",
-    "url": "/Voting-System/static/css/main.730fdd82.chunk.css"
+    "revision": "a052694c57ee6d284b49",
+    "url": "/Voting-System/static/css/main.4a0f2570.chunk.css"
   },
   {
     "revision": "282e0c4e1de7da7d7d71",
     "url": "/Voting-System/static/js/2.ad92664a.chunk.js"
   },
   {
-    "revision": "e44accd67aff01e00abb",
+    "revision": "a052694c57ee6d284b49",
     "url": "/Voting-System/static/js/main.25a4a20b.chunk.js"
   },
   {
     "revision": "093f317a891ed4e6aafc",
     "url": "/Voting-System/static/js/runtime-main.5655bdc1.js"
+  },
+  {
+    "revision": "e80347f8b1766c4b233b8caad52bc648",
+    "url": "/Voting-System/static/media/Dark-Website.e80347f8.jpg"
   }
 ]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Voting-System/precache-manifest.10c25ee1483a7303d76a09dc71a4ce94.js"
+  "/Voting-System/precache-manifest.53570f29256afdc8f6434767f21148a1.js"
 );
 
 self.addEventListener('message', (event) => {
